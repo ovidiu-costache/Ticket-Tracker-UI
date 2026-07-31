8698057f0b207eac2cb3485f8d1540bac72bbc57
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2026-07-31 12:10:35 +0300

    Ex5: Add custom pipes and apply built-in pipes

--- src/app/ticket-card/ticket-card.ts
+++ src/app/ticket-card/ticket-card.ts
@@ -1,8 +1,11 @@
+import { DatePipe, TitleCasePipe } from '@angular/common';
 import { Component, Output, Input, EventEmitter } from '@angular/core';
+import { StatusLabelPipe } from '../status-label-pipe';
+import { PriorityLabelPipe } from '../priority-label-pipe';
 
 @Component({
   selector: 'app-ticket-card',
-  imports: [],
+  imports: [DatePipe, TitleCasePipe, StatusLabelPipe, PriorityLabelPipe],
   templateUrl: './ticket-card.html',
   styleUrl: './ticket-card.css',
 })
