541f92845cfd59347c32aa2281bb57542be5a6ff
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2026-08-05 10:32:16 +0300

    Ex7: Implement reactive forms with validation for ticket creation

--- src/app/ticket-list/ticket-list.ts
+++ src/app/ticket-list/ticket-list.ts
@@ -1,19 +1,32 @@
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
 import { TicketCardComponent } from "../ticket-card/ticket-card";
 import { TicketService } from '../ticket.service';
 
 @Component({
   selector: 'app-ticket-list',
-  imports: [TicketCardComponent, FormsModule],
+  imports: [TicketCardComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './ticket-list.html',
   styleUrl: './ticket-list.css',
 })
 export class TicketList implements OnInit {
   tickets: any[] = [];
-  searchText = '';
-  title = '';
-  priority = 1;
+  searchText = ''; // Ramane ngModel pt search
+  
+  // Formularul FormGroup si campurile FormControl
+  ticketForm = new FormGroup({
+    title: new FormControl('', [
+      Validators.required,
+      Validators.maxLength(100)  
+    ]),
+    description: new FormControl('', [
+      Validators.maxLength(1000)
+    ]),
+    priorityId: new FormControl(1, [
+      Validators.required,
+      Validators.pattern('^[1-3]$') // Ca sa accepte doar 1, 2 sau 3
+    ])
+  });
 
   constructor(private ticketService: TicketService) {}
 
@@ -34,22 +47,27 @@ export class TicketList implements OnInit {
     return this.tickets.filter(ticket => ticket.title.toLowerCase().includes(this.searchText.toLowerCase()));
   }
 
+  // Refactor
   addTicket() {
+    if (this.ticketForm.invalid) {
+      return; 
+    }
+
     const newTicket = {
       id: this.tickets.length + 1,
       ticketKey: 'TK-' + (this.tickets.length + 201),
-      title: this.title,
+      title: this.ticketForm.value.title,
       description: '',
       createdAt: new Date(),
       statusId: 1,
-      priorityId: Number(this.priority)
+      priorityId: Number(this.ticketForm.value.priorityId)
     };
 
     this.ticketService.addTicket(newTicket);
     this.loadTickets();
 
-    this.title = '';
-    this.priority = 1;
+    // Resetare
+    this.ticketForm.reset({ priorityId: 1, title: '', description: '' });
   }
 
   clearList() {
