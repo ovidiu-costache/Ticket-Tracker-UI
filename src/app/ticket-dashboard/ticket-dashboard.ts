69d350f600ece6c50dcedb7b40a94eba4de1b2c5
ovidiu-costache <ovidiu.costache2110@stud.acs.upb.ro>
2026-08-05 12:37:47 +0300

    Improve UX by highlighting active filter and adding contextual empty state warning

--- src/app/ticket-dashboard/ticket-dashboard.ts
+++ src/app/ticket-dashboard/ticket-dashboard.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TicketService } from '../ticket.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-ticket-dashboard',
@@ -10,10 +11,16 @@ import { TicketService } from '../ticket.service';
 export class TicketDashboard implements OnInit {
   counts: any = {};
 
+  // Ce filtru e selectat acum
+  currentFilter: number | null = null;
+  filterSub!: Subscription;
+
   constructor(private ticketService: TicketService) {}
 
   ngOnInit() {
     this.ticketService.getTicketCount().subscribe(data => {this.counts = data});
+
+    this.filterSub = this.ticketService.currentFilter$.subscribe(filterValue => {this.currentFilter = filterValue;});
   }
 
   setFilter(statusId: number | null) {
